24bbc3d98bb2179eb97712da6f055b3bd50ba09f
sisig <61018516+c-sig@users.noreply.github.com>
2023-03-10 05:28:52 +0800

    moved commands to command folder

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from pathlib import Path
 from discord import app_commands
 from discord.ext import commands
 from config import *
-from permissions import permissiongroup
+from commands import permissions
+
 
 config_file, config, bot, group_name, token, owners, managers, members, task_path = initializebot()
 
@@ -16,6 +17,6 @@ async def sync(ctx):
     await bot.tree.sync()
     print('done')
 
-bot.tree.add_command(permissiongroup)
+bot.tree.add_command(permissions.permissiongroup)
 
 bot.run(token)
