11d5d1f6d255c7edf2fa8f8c689f3f3662160592
sisig <61018516+c-sig@users.noreply.github.com>
2023-03-14 06:16:29 +0800

    Update boards.py

--- commands/boards.py
+++ commands/boards.py
@@ -6,15 +6,18 @@ boardgroup = app_commands.Group(name='board', description='Board commands.')
 CONFIGS_FOLDER = 'configs'
 database_path = os.path.join(CONFIGS_FOLDER, 'database.yaml')
 
+
 def loadboards():
     with open(database_path, 'r') as file:
         boards = yaml.safe_load(file)
     return boards
 
+
 def autocomplete(choices: list, current: str):
     if not current:
         return choices
     return [ch for ch in choices if ch.name.lower().startswith(current.lower())] or choices
+
 
 @boardgroup.command(name='create', description='Creates a board.')
 async def create_board(interaction: discord.Interaction, boardname: str):
@@ -36,6 +39,7 @@ async def create_board(interaction: discord.Interaction, boardname: str):
 
     await interaction.response.send_message(f'{boardname} has been created.')
 
+
 @boardgroup.command(name='delete', description='Deletes an existing board')
 async def delete_board(interaction: discord.Interaction, boardname: str):
     # Load board list from database
@@ -53,12 +57,14 @@ async def delete_board(interaction: discord.Interaction, boardname: str):
 
     await interaction.response.send_message(f'{boardname} has been deleted.')
 
+
 @delete_board.autocomplete('boardname')
 async def deleteautocomplete(interaction: discord, current: str):
     boards = loadboards()
     choices = [app_commands.Choice(name=name, value=name) for name in boards.keys()]
     filtered_choices = [choice for choice in choices if choice.name.startswith(current)]
     return filtered_choices
+
 
 @boardgroup.command(name='rename', description='Renames an existing board')
 async def rename_board(interaction: discord.Interaction, oldname: str, newname: str):
@@ -83,12 +89,14 @@ async def rename_board(interaction: discord.Interaction, oldname: str, newname: 
 
     await interaction.response.send_message(f'{oldname} has been renamed to {newname}.')
 
+
 @rename_board.autocomplete('oldname')
 async def renameautocomplete(interaction: discord, current: str):
     boards = loadboards()
     choices = [app_commands.Choice(name=name, value=name) for name in boards.keys()]
     filtered_choices = [choice for choice in choices if choice.name.startswith(current)]
     return filtered_choices
+
 
 @boardgroup.command(name='list', description='Lists all available boards')
 async def list_board(interaction: discord.Interaction):
